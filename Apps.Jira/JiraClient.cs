98c5e2b6889e0a3a19bbb7ecba9cc782789cb23d
Olha Sokolenko <solgav12@gmail.com>
2023-07-05 13:17:28 +0300

    used RestClient instead of HttpClient

--- Apps.Jira/JiraClient.cs
+++ Apps.Jira/JiraClient.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using Apps.Jira.Dtos;
+﻿using Apps.Jira.Dtos;
 using Blackbird.Applications.Sdk.Common.Authentication;
 using RestSharp;
 
@@ -9,24 +8,22 @@ namespace Apps.Jira
     {
         private static Uri GetUri(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders)
         {
-            var cloudId = GetJiraCloudId(authenticationCredentialsProviders).Result;
+            var cloudId = GetJiraCloudId(authenticationCredentialsProviders);
             var uri = $"https://api.atlassian.com/ex/jira/{cloudId}/rest/api/3";
             return new Uri(uri);
         }
 
-        private static async Task<string> GetJiraCloudId(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders)
+        private static string GetJiraCloudId(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders)
         {
             const string atlassianResourcesUrl = "https://api.atlassian.com/oauth/token/accessible-resources";
             string jiraUrl = authenticationCredentialsProviders.First(p => p.KeyName == "jira_url").Value;
-            string authorizationHeader =
-                    authenticationCredentialsProviders.First(p => p.KeyName == "Authorization").Value;
-            using HttpClient httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Add("Authorization", authorizationHeader);
-            using var response = await httpClient.GetAsync(atlassianResourcesUrl);
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var atlassianCloudResources = JsonSerializer.Deserialize<List<AtlassianCloudResourceDto>>(responseContent) 
-                                ?? throw new InvalidOperationException($"Invalid response content: {responseContent}"); ;
-            var cloudId = atlassianCloudResources.First(jiraResource => jiraUrl.Contains(jiraResource.Url)).Id;
+            string authorizationHeader = authenticationCredentialsProviders.First(p => p.KeyName == "Authorization").Value;
+            var restClient = new RestClient(new RestClientOptions { ThrowOnAnyError = true, BaseUrl = new Uri(atlassianResourcesUrl) });
+            var request = new RestRequest("", Method.Get);
+            request.AddHeader("Authorization", authorizationHeader);
+            var atlassianCloudResources = restClient.Get<List<AtlassianCloudResourceDto>>(request);
+            var cloudId = atlassianCloudResources.First(jiraResource => jiraUrl.Contains(jiraResource.Url)).Id
+                          ?? throw new ArgumentException("The Jira URL is incorrect.");
             return cloudId;
         }
 
