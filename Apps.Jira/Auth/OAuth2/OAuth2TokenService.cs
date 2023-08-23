392c2f0a872bb7e123a9f89027831bf62fb592d5
sokolenkoolha <olha.sokolenko@blackbird.io>
2023-08-23 10:14:32 +0300

    added refresh token method

--- Apps.Jira/Auth/OAuth2/OAuth2TokenService.cs
+++ Apps.Jira/Auth/OAuth2/OAuth2TokenService.cs
@@ -6,16 +6,23 @@ namespace Apps.Jira.Auth.OAuth2
     public class OAuth2TokenService : IOAuth2TokenService
     {
         private const string AtlassianTokenUrl = "https://auth.atlassian.com/oauth/token";
+        private const string ExpiresAtKeyName = "expires_at";
 
-        public bool IsRefreshToken(Dictionary<string, string> values)
-        {
-            return false;
-        }
+        public bool IsRefreshToken(Dictionary<string, string> values) 
+            => values.TryGetValue(ExpiresAtKeyName, out var expireValue) && DateTime.UtcNow > DateTime.Parse(expireValue);
 
         public async Task<Dictionary<string, string>> RefreshToken(Dictionary<string, string> values,
             CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            const string grantType = "refresh_token";
+            var bodyParameters = new Dictionary<string, string>
+            {
+                { "grant_type", grantType },
+                { "client_id", ApplicationConstants.ClientId },
+                { "client_secret", ApplicationConstants.ClientSecret },
+                { "refresh_token", values["refresh_token"] }
+            };
+            return await RequestToken(bodyParameters, cancellationToken);
         }
 
         public async Task<Dictionary<string, string?>> RequestToken(
@@ -43,13 +50,17 @@ namespace Apps.Jira.Auth.OAuth2
         private async Task<Dictionary<string, string>> RequestToken(Dictionary<string, string> bodyParameters,
             CancellationToken cancellationToken)
         {
+            var utcNow = DateTime.UtcNow;
             using HttpClient httpClient = new HttpClient();
             using var httpContent = new FormUrlEncodedContent(bodyParameters);
             using var response = await httpClient.PostAsync(AtlassianTokenUrl, httpContent, cancellationToken);
-            var responseContent = await response.Content.ReadAsStringAsync();
+            var responseContent = await response.Content.ReadAsStringAsync(cancellationToken);
             var resultDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(responseContent)?
                                        .ToDictionary(r => r.Key, r => r.Value?.ToString())
                 ?? throw new InvalidOperationException($"Invalid response content: {responseContent}");
+            var expiresIn = int.Parse(resultDictionary["expires_in"]);
+            var expiresAt = utcNow.AddSeconds(expiresIn);
+            resultDictionary.Add(ExpiresAtKeyName, expiresAt.ToString());
             return resultDictionary;
         }
     }
