d4700e228d1b4bdc8537fd3820ba4e4e2d5d353d
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-11-01 10:33:32 +0200

    Artifactory

--- Apps.Jira/Auth/OAuth2/OAuth2AuthorizeService.cs
+++ Apps.Jira/Auth/OAuth2/OAuth2AuthorizeService.cs
@@ -1,24 +1,33 @@
-﻿using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
+﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
+using Blackbird.Applications.Sdk.Common.Invocation;
 using Microsoft.AspNetCore.WebUtilities;
 
 namespace Apps.Jira.Auth.OAuth2
 {
-    public class OAuth2AuthorizeService : IOAuth2AuthorizeService
+    public class OAuth2AuthorizeService : BaseInvocable, IOAuth2AuthorizeService
     {
+        public OAuth2AuthorizeService(InvocationContext invocationContext) : base(invocationContext)
+        {
+        }
+
         public string GetAuthorizationUrl(Dictionary<string, string> values)
         {
+            string bridgeOauthUrl = $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/oauth";
             const string atlassianAuthorizeUrl = "https://auth.atlassian.com/authorize";
             var parameters = new Dictionary<string, string>
             {
                 { "audience", "api.atlassian.com" },
                 { "client_id", ApplicationConstants.ClientId },
                 { "scope", ApplicationConstants.Scopes },
-                { "redirect_uri", ApplicationConstants.RedirectUri },
+                { "redirect_uri", $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/AuthorizationCode" },
                 { "state", values["state"] },
                 { "response_type", "code" },
-                { "prompt", "consent" }
+                { "prompt", "consent" },
+                { "authorization_url", bridgeOauthUrl},
+                { "actual_redirect_uri", InvocationContext.UriInfo.AuthorizationCodeRedirectUri.ToString() },
             };
-            return QueryHelpers.AddQueryString(atlassianAuthorizeUrl, parameters);
+            return QueryHelpers.AddQueryString(bridgeOauthUrl, parameters);
         }
     }
 }
--- Apps.Jira/Webhooks/Handlers/IssueHandlers/IssueUpdatedHandler.cs
+++ Apps.Jira/Webhooks/Handlers/IssueHandlers/IssueUpdatedHandler.cs
@@ -1,9 +1,11 @@
-﻿namespace Apps.Jira.Webhooks.Handlers.IssueHandlers
+﻿using Blackbird.Applications.Sdk.Common.Invocation;
+
+namespace Apps.Jira.Webhooks.Handlers.IssueHandlers
 {
     public class IssueUpdatedHandler : BaseWebhookHandler
     {
         private static readonly string[] _subscriptionEvents = { "jira:issue_updated" };
         
-        public IssueUpdatedHandler() : base(_subscriptionEvents) { }
+        public IssueUpdatedHandler(InvocationContext invocationContext) : base(invocationContext, _subscriptionEvents) { }
     }
 }
