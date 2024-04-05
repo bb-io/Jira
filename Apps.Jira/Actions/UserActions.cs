0f157f73732bb7cff60933fb8eb0660a590111a0
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-05 11:29:38 +0300

    Implemented user actions

--- Apps.Jira/Actions/UserActions.cs
+++ Apps.Jira/Actions/UserActions.cs
@@ -1,4 +1,5 @@
-﻿using Apps.Jira.Models.Identifiers;
+﻿using Apps.Jira.Dtos;
+using Apps.Jira.Models.Identifiers;
 using Apps.Jira.Models.Requests;
 using Apps.Jira.Models.Responses;
 using Apps.Jira.Utils;
@@ -6,6 +7,7 @@ using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using RestSharp;
+using UserDto = Apps.Jira.Models.Responses.UserDto;
 
 namespace Apps.Jira.Actions;
 
@@ -25,10 +27,10 @@ public class UserActions : JiraInvocable
     }
     
     [Action("Get user", Description = "Get the specified user.")]
-    public async Task<UserDto> GetUser([ActionParameter] UserIdentifier input)
+    public async Task<ExpandedUserDto> GetUser([ActionParameter] UserIdentifier input)
     {
         var request = new JiraRequest($"/user?accountId={input.AccountId}", Method.Get);
-        var user = await Client.ExecuteWithHandling<UserDto>(request);
+        var user = await Client.ExecuteWithHandling<ExpandedUserDto>(request);
         return user;
     }
     
@@ -52,5 +54,58 @@ public class UserActions : JiraInvocable
         
         var user = await Client.ExecuteWithHandling<UserDto>(request);
         return user;
+    }
+    
+    [Action("Get groups", Description = "Get the groups for the specified user.")]
+    public async Task<List<GroupDto>> GetGroups([ActionParameter] UserIdentifier input)
+    {
+        var request = new JiraRequest($"/user/groups?accountId={input.AccountId}", Method.Get);
+        var groups = await Client.ExecuteWithHandling<List<GroupDto>>(request);
+        return groups;
+    }
+    
+    [Action("Get user email", Description = "Get the email for the specified user.")]
+    public async Task<UserEmailDto> GetUserEmail([ActionParameter] UserIdentifier input)
+    {
+        var request = new JiraRequest($"/user/email?accountId={input.AccountId}", Method.Get);
+        var email = await Client.ExecuteWithHandling<UserEmailDto>(request);
+        return email;
+    }
+    
+    [Action("Get user columns", Description = "Get the columns for the specified user.")]
+    public async Task<List<ColumnDto>> GetUserColumns([ActionParameter] UserIdentifier input)
+    {
+        var request = new JiraRequest($"/user/columns?accountId={input.AccountId}", Method.Get);
+        var columns = await Client.ExecuteWithHandling<List<ColumnDto>>(request);
+        return columns;
+    }
+    
+    [Action("Reset user default columns", Description = "Reset the default columns for the specified user.")]
+    public async Task ResetUserDefaultColumns([ActionParameter] UserIdentifier input)
+    {
+        var request = new JiraRequest($"/user/columns?accountId={input.AccountId}", Method.Delete);
+        await Client.ExecuteWithHandling(request);
+    }
+    
+    [Action("Set user columns", Description = "Set the columns for the specified user.")]
+    public async Task SetUserColumns([ActionParameter] UserIdentifier input, [ActionParameter] SetUserColumns columnsRequest)
+    {
+        var request = new JiraRequest($"/user/columns?accountId={input.AccountId}", Method.Put)
+            .AddJsonBody(new
+            {
+                columns = columnsRequest.Columns.ToList()
+            });
+        
+        await Client.ExecuteWithHandling(request);
+    }
+    
+    [Action("Bulk get users", Description = "Bulk get users.")]
+    public async Task<List<BulkUserDto>> BulkGetUsers([ActionParameter] IEnumerable<UserIdentifier> inputs)
+    {
+        var request = new JiraRequest("/users/bulk", Method.Post)
+            .AddJsonBody(inputs.Select(x => new { accountId = x.AccountId }));
+        
+        var users = await Client.ExecuteWithHandling<List<BulkUserDto>>(request);
+        return users;
     }
 }
--- Apps.Jira/Models/Responses/UserDto.cs
+++ Apps.Jira/Models/Responses/UserDto.cs
@@ -1,21 +1,38 @@
-﻿namespace Apps.Jira.Models.Responses;
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.Jira.Models.Responses;
 
 public class UserDto
 {
+    [Display("Account ID")]
     public string AccountId { get; set; }
+    
+    [Display("Account ID")]
     public string AccountType { get; set; }
+    
     public bool Active { get; set; }
+    
     public AvatarUrls AvatarUrls { get; set; }
     public string DisplayName { get; set; }
+    
     public string Key { get; set; }
+    
     public string Name { get; set; }
+    
     public string Self { get; set; }
 }
 
 public class AvatarUrls
 {
+    [Display("Size 16")]
     public string Size16 { get; set; }
+    
+    [Display("Size 24")]
     public string Size24 { get; set; }
+    
+    [Display("Size 32")]
     public string Size32 { get; set; }
+    
+    [Display("Size 48")]
     public string Size48 { get; set; }
 }
