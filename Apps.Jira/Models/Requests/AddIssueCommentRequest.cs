2cc8223adbbdb9d0147d83c3376b2f9e94270363
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-05 13:26:40 +0300

    Issue comment actions

---
+++ Apps.Jira/Models/Requests/AddIssueCommentRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace Apps.Jira.Models.Requests;
+
+public class AddIssueCommentRequest
+{
+    public string Text { get; set; }
+
+    public string? ContentType { get; set; }
+        
+    public string? Type { get; set; }
+
+    public string? BodyType { get; set; }
+        
+    public string? Version { get; set; }
+        
+    public string? VisibilityType { get; set; }
+        
+    public string? VisibilityValue { get; set; }
+        
+    public string? VisibilityIdentifier { get; set; }
+}
--- Apps.Jira/Models/Responses/UserDto.cs
+++ Apps.Jira/Models/Responses/UserDto.cs
@@ -1,4 +1,5 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Newtonsoft.Json;
 
 namespace Apps.Jira.Models.Responses;
 
@@ -7,32 +8,31 @@ public class UserDto
     [Display("Account ID")]
     public string AccountId { get; set; }
     
-    [Display("Account ID")]
+    [Display("Account type")]
     public string AccountType { get; set; }
     
     public bool Active { get; set; }
     
+    [Display("Avatar URLs")]
     public AvatarUrls AvatarUrls { get; set; }
+    
+    [Display("Display name")]
     public string DisplayName { get; set; }
-    
-    public string Key { get; set; }
-    
-    public string Name { get; set; }
     
     public string Self { get; set; }
 }
 
 public class AvatarUrls
 {
-    [Display("Size 16")]
+    [Display("Size 16"), JsonProperty("16x16")]
     public string Size16 { get; set; }
     
-    [Display("Size 24")]
+    [Display("Size 24"), JsonProperty("24x24")]
     public string Size24 { get; set; }
     
-    [Display("Size 32")]
+    [Display("Size 32"), JsonProperty("32x32")]
     public string Size32 { get; set; }
     
-    [Display("Size 48")]
+    [Display("Size 48"), JsonProperty("48x48")]
     public string Size48 { get; set; }
 }
