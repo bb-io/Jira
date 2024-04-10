f32ac66449ab3fe98cd66fc37253498d5c1c3426
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-10 12:04:43 +0300

    added categories

--- Apps.Jira/JiraApplication.cs
+++ Apps.Jira/JiraApplication.cs
@@ -2,12 +2,19 @@
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Jira
 {
-    internal class JiraApplication : BaseInvocable, IApplication
+    internal class JiraApplication : BaseInvocable, IApplication, ICategoryProvider
     {
         private readonly Dictionary<Type, object> _typesInstances;
+
+        public IEnumerable<ApplicationCategory> Categories
+        {
+            get => [ApplicationCategory.ProjectManagementAndProductivity, ApplicationCategory.TaskManagement];
+            set { }
+        }
 
         public JiraApplication(InvocationContext invocationContext) : base(invocationContext)
         {
@@ -26,9 +33,10 @@ namespace Apps.Jira
             {
                 throw new InvalidOperationException($"Instance of type '{typeof(T)}' not found");
             }
+
             return (T)value;
         }
-        
+
         private Dictionary<Type, object> CreateTypesInstances()
         {
             return new Dictionary<Type, object>
