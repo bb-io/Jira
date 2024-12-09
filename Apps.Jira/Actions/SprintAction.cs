70148cb49917cadfe2be3f4684df29fc3cb183d1
RiabushenkoA <ariabushenko@blackbird.io>
2024-12-09 19:53:04 +0200

    version control

--- Apps.Jira/Actions/SprintAction.cs
+++ Apps.Jira/Actions/SprintAction.cs
@@ -10,39 +10,39 @@ using RestSharp;
 
 namespace Apps.Jira.Actions
 {
-    [ActionList]
-    public class SprintActions(InvocationContext invocationContext, IFileManagementClient fileManagementClient)
-    : JiraInvocable(invocationContext)
-    {
+    //[ActionList]
+    //public class SprintActions(InvocationContext invocationContext, IFileManagementClient fileManagementClient)
+    //: JiraInvocable(invocationContext)
+    //{
 
-        [Action("Get relevant sprint for date", Description = "Get Sprint corresponding to the specified date for a selected board.")]
-        public async Task<SprintsResponse> GetRelevantSprintForDate(
-            [ActionParameter] GetSprintByDateRequest requestModel)
-        {
+    //    [Action("Get relevant sprint for date", Description = "Get Sprint corresponding to the specified date for a selected board.")]
+    //    public async Task<SprintsResponse> GetRelevantSprintForDate(
+    //        [ActionParameter] GetSprintByDateRequest requestModel)
+    //    {
 
-            var endpoint = $"/rest/agile/1.0/board/{requestModel.BoardId}/sprint";
-            var request = new JiraRequest(endpoint, Method.Get);
-            var response = await Client.ExecuteWithHandling<SprintsWrapper>(request);
+    //        var endpoint = $"/rest/agile/1.0/board/{requestModel.BoardId}/sprint";
+    //        var request = new JiraRequest(endpoint, Method.Get);
+    //        var response = await Client.ExecuteWithHandling<SprintsWrapper>(request);
 
-            var relevantSprints = response.Values
-                .Where(sprint =>
-                    sprint.StartDate <= requestModel.Date && sprint.EndDate >= requestModel.Date)
-                .ToList();
+    //        var relevantSprints = response.Values
+    //            .Where(sprint =>
+    //                sprint.StartDate <= requestModel.Date && sprint.EndDate >= requestModel.Date)
+    //            .ToList();
 
-            if (!relevantSprints.Any())
-                return new SprintsResponse
-                {
-                    Message = $"No sprints found for the date {requestModel.Date.ToShortDateString()}.",
-                    Sprints = new List<SprintDto>()
-                };
+    //        if (!relevantSprints.Any())
+    //            return new SprintsResponse
+    //            {
+    //                Message = $"No sprints found for the date {requestModel.Date.ToShortDateString()}.",
+    //                Sprints = new List<SprintDto>()
+    //            };
 
-            return new SprintsResponse
-            {
-                Message = $"Found {relevantSprints.Count} relevant sprint(s) for the date {requestModel.Date.ToShortDateString()}.",
-                Sprints = relevantSprints.Select(s => new SprintDto(s)).ToList()
-            };
-        }
+    //        return new SprintsResponse
+    //        {
+    //            Message = $"Found {relevantSprints.Count} relevant sprint(s) for the date {requestModel.Date.ToShortDateString()}.",
+    //            Sprints = relevantSprints.Select(s => new SprintDto(s)).ToList()
+    //        };
+    //    }
 
 
-    }
+    //}
 }
