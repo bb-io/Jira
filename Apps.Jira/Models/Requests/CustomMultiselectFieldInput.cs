ca5e1e59ca3ddfd4a952b406c2ff4f9a5388fbf6
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-04 11:14:47 +0200

    Changed naming input

--- Apps.Jira/Models/Requests/CustomMultiselectFieldInput.cs
+++ Apps.Jira/Models/Requests/CustomMultiselectFieldInput.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Blackbird.Applications.Sdk.Common;
 
 namespace Apps.Jira.Models.Requests
 {
     public class CustomMultiselectFieldInput
     {
+        [Display("Multiselect value field")]
         public IEnumerable<string> ValueProperty { get; set; }
     }
 }
