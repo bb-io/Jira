4f1c7cdc7be22843e044e28dd6570eb18cf2bbad
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-06-20 12:18:30 +0300

    fix(ApplicationConstants): ✏️ Update `Scopes` to include `manage:jira-configuration` permission

--- Apps.Jira/ApplicationConstants.cs
+++ Apps.Jira/ApplicationConstants.cs
@@ -4,6 +4,6 @@ public class ApplicationConstants
 {
     public const string ClientId = "#{JIRA_CLIENT_ID}#";
     public const string ClientSecret = "#{JIRA_SECRET}#";
-    public const string Scopes = "read:email-address:jira read:me read:sprint:jira-software read:jira-work write:jira-work read:jira-user offline_access read:board-scope:jira-software read:project:jira write:sprint:jira-software";
+    public const string Scopes = "read:email-address:jira read:me read:sprint:jira-software read:jira-work write:jira-work read:jira-user offline_access read:board-scope:jira-software read:project:jira write:sprint:jira-software manage:jira-configuration read:issue-event:jira";
     public const string BlackbirdToken = "#{JIRA_BLACKBIRD_TOKEN}#";
 }
