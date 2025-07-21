9fe8e0e38fbdb28ab99c9c0258a5f00ab02abf91
mathijs-bb <mathijs.sonnemans@blackbird.io>
2025-07-21 11:11:56 +0200

    Added ID display names

--- Apps.Jira/Dtos/PriorityDto.cs
+++ Apps.Jira/Dtos/PriorityDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Apps.Jira.Dtos;
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.Jira.Dtos;
 
 public class PriorityDto
 {
+    [Display("Priority ID")]
     public string Id { get; set; }
     public string Name { get; set; }
 }
