681edeb2184fd861a111870d87e141dff294fe89
ce-nist <cecilia.nistal@blackbird.io>
2025-12-03 14:16:02 -0300

    Added Find issue comment by text action

---
+++ Apps.Jira/Models/Requests/FindCommentRequest.cs
@@ -0,0 +1,11 @@
+﻿using Apps.Jira.Models.Identifiers;
+using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.Jira.Models.Requests;
+
+public class FindCommentRequest : IssueIdentifier
+{
+    [Display("Comment text contains")]
+
+    public string CommentContains { get; set; }
+}
